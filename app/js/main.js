e5182fc85acc2ad0afe4fe1cdd21ae6db7de2319
Oleksii Teplenko <pytnik.tov@gmail.com>
2016-07-26 23:37:37 +0300

    social-icons justified

--- app/js/main.js
+++ app/js/main.js
@@ -6,26 +6,30 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		password 		= document.getElementById('password'),
 		signInButton 	= document.querySelector('.btn-sign-in');
 
-	var regName 		= /^[a-z0-9_-]{3,16}$/,
+	var regNamePhone 	= /^\+380[0-9]{9}$/,
+		regNameEmail 	= /^([a-zA-Z0-9_\.-]+)@([a-zA-Z0-9_\.-]+)\.([a-zA-Z\.]{2,6})$/,
 		regPassword 	= /^[a-zA-Z0-9_-]{6,18}$/;
 
+	function tooltip(item, msg) {
+		
+	}
 
 	signInButton.onclick = function() {
-		if ( !regName.test(name.value) ) {
+		if ( !regNamePhone.test(name.value) && !regNameEmail.test(name.value) ) {
 			alert('Будь ласка, введіть ім`я!\nІм`я повинно складатися не менше ніж із 3-х символів і не містити спецсимволи!');
 			name.focus();
 			name.style.borderColor = 'red';
-			console.dir(name);
 			return;
 		} else if ( !regPassword.test(password.value) ) {
+			name.style.borderColor = 'green';
 			alert('Будь ласка, введіть пароль!\nПароль повинен бути не менше із 6-ти симфолів і не містити спецсимволи!');
 			password.focus();
 			password.style.borderColor = 'red';
 			return;
 		}
 
-		name.style.borderColor = '';
-		password.style.borderColor = '';
+		password.style.borderColor = 'green';
+
 		authForm.submit();
 	};
 });
