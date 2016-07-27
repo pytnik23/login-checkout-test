cf71a961aa447a39d36bc16083a588a1e4c7bf74
Oleksii Teplenko <pytnik.tov@gmail.com>
2016-07-27 10:16:06 +0300

    added tooltips

--- app/js/main.js
+++ app/js/main.js
@@ -10,25 +10,34 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		regNameEmail 	= /^([a-zA-Z0-9_\.-]+)@([a-zA-Z0-9_\.-]+)\.([a-zA-Z\.]{2,6})$/,
 		regPassword 	= /^[a-zA-Z0-9_-]{6,18}$/;
 
-	function tooltip(item, msg) {
-		
+	function tooltip(elem, msg) {
+		var div = document.createElement('div');
+		div.classList.add('input-tooltip');
+		div.setAttribute('title', msg);
+	
+		elem.parentNode.insertBefore(div, elem.nextSibling);
 	}
 
 	signInButton.onclick = function() {
 		if ( !regNamePhone.test(name.value) && !regNameEmail.test(name.value) ) {
-			alert('Будь ласка, введіть ім`я!\nІм`я повинно складатися не менше ніж із 3-х символів і не містити спецсимволи!');
+			var msg = 'Будь ласка, введіть Email або телефон у форматі +380XXXXXXXXX';
+			tooltip(name, msg);
 			name.focus();
 			name.style.borderColor = 'red';
 			return;
 		} else if ( !regPassword.test(password.value) ) {
 			name.style.borderColor = 'green';
-			alert('Будь ласка, введіть пароль!\nПароль повинен бути не менше із 6-ти симфолів і не містити спецсимволи!');
+			name.parentNode.removeChild(document.querySelector('.input-tooltip'));
+
+			var msg = 'Будь ласка, введіть пароль! Пароль повинен бути не менше із 6-ти симфолів і не містити спецсимволи!';
+			tooltip(password, msg);
 			password.focus();
 			password.style.borderColor = 'red';
 			return;
 		}
 
 		password.style.borderColor = 'green';
+		name.parentNode.removeChild(document.querySelector('.input-tooltip'));
 
 		authForm.submit();
 	};
